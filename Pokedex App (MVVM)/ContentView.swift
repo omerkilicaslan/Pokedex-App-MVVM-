5bd3cea630518081939fc12defbe7bcc62b2e247
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-24 16:13:33 +0300

    Initial Commit

---
+++ Pokedex App (MVVM)/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pokedex App (MVVM)
+//
+//  Created by Ömer Faruk Kılıçaslan on 24.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pokedex App (MVVM)/Pokedex_App__MVVM_App.swift
@@ -0,0 +1,17 @@
+//
+//  Pokedex_App__MVVM_App.swift
+//  Pokedex App (MVVM)
+//
+//  Created by Ömer Faruk Kılıçaslan on 24.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct Pokedex_App__MVVM_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
